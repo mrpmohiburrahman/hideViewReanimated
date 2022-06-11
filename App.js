d73fb830f7aeecb61b72c7fc3b702934f4f091cd
mrpmohiburrahman <mrpmohiburrahman@gmail.com>
2022-06-11 16:03:02 +0600

    implement hide view using reanimated

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// @ts-nocheck
+import { useState } from "react";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
+
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 export default function App() {
+  const toggleIsVisible = () => {
+    setIsVisible(false);
+    setTimeout(() => setIsVisible(true), 1000);
+  };
+
+  const [isVisible, setIsVisible] = useState(true);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    height: isVisible ? withTiming(100, 1000) : withTiming(0, 1000),
+  }));
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View style={[styles.animatedViewContainer, animatedStyle]}>
+        <TouchableOpacity onPress={toggleIsVisible}>
+          <Text style={styles.textStyle}>{`Subscribe`}</Text>
+        </TouchableOpacity>
+      </Animated.View>
     </View>
   );
 }
@@ -13,8 +41,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FAF5E4",
+    paddingTop: SCREEN_HEIGHT * 0.5,
+    paddingLeft: SCREEN_WIDTH * 0.25,
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  animatedViewContainer: {
+    backgroundColor: "#F47C7C",
+    height: 100,
+    width: 200,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textStyle: {
+    color: "#180A0A",
+    fontSize: 20,
   },
 });
